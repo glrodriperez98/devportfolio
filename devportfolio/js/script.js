77f7831f4d7ec7e41105809e5cee6c4df9161ad5
Gabriel Perez <gabrielrperez47@gmail.com>
2025-05-06 09:48:09 +0100

    Deployement of devportfolio

---
+++ devportfolio/js/script.js
@@ -0,0 +1,40 @@
+// Hide and show nav bar when scrolling to the about page
+document.addEventListener("DOMContentLoaded", function () {
+    let navbar = document.getElementById("nav-bar");
+    let firstSection = document.getElementById("about");
+
+    document.addEventListener("scroll", function () {
+        if (window.scrollY > firstSection.clientHeight - 50) {
+            navbar.style.top = "0";
+        } else {
+            navbar.style.top = "-60px";
+        }
+    });
+});
+
+// Highlight active navbar link when scrolling to a section and remove highlight when moving to another
+document.addEventListener("DOMContentLoaded", function () {  
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll(".nav-selectors");
+
+    function changeActiveLink() {
+        let fromTop = window.scrollY;
+
+        sections.forEach((section) => {
+            let sectionTop = section.offsetTop - 60;
+            let sectionHeight = section.offsetHeight;
+            let sectionID = section.getAttribute("id");
+
+            if (fromTop >= sectionTop && fromTop < sectionTop + sectionHeight) {
+                navLinks.forEach((link) => link.classList.remove("active"));
+
+                let activeLink = document.querySelector(`.nav-selectors[href="#${sectionID}"]`);
+                if (activeLink) {
+                    activeLink.classList.add("active");
+                }
+            }
+        });
+    }
+
+    window.addEventListener("scroll", changeActiveLink);
+});
